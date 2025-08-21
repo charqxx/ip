42835b777fea53a2ec2cc16310510b5bba62ae40
Charlize <E1354638@u.nus.edu.sg>
2025-08-22 02:05:32 +0800

    Added SereneException class

---
+++ src/main/java/SereneException.java
@@ -0,0 +1,10 @@
+public class SereneException extends Exception{
+    public SereneException(String message) {
+        super(message);
+    }
+    @Override
+    public String toString() {
+        return super.getMessage();
+    }
+}
+

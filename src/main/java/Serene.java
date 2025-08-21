7d3ae108795d698339befab691bdb1d77200e9e6
Charlize <E1354638@u.nus.edu.sg>
2025-08-22 01:20:43 +0800

    Added support for echo

--- src/main/java/Serene.java
+++ src/main/java/Serene.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 public class Serene {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Hello! I'm Serene\nWhat can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        while(true) {
+            String input = sc.nextLine();
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+
+
     }
 }

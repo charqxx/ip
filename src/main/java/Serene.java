c77f71e1a2d932c30bde01f12ac209dfb5e83f21
Charlize <E1354638@u.nus.edu.sg>
2025-08-22 01:43:05 +0800

    Added support for deadline

--- src/main/java/Serene.java
+++ src/main/java/Serene.java
@@ -33,6 +33,16 @@ public class Serene {
                 System.out.println(newTask);
                 String message = String.format("Now you have %d tasks in the list.", history.size());
                 System.out.println(message);
+            } else if (parts[0].equals("deadline")) {
+                String taskWithDate = input.split(" ", 2)[1];
+                String task = taskWithDate.split(" /by ")[0];
+                String date = taskWithDate.split(" /by ")[1];
+                Deadline newTask = new Deadline(task, date);
+                history.add(newTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println(newTask);
+                String message = String.format("Now you have %d tasks in the list.", history.size());
+                System.out.println(message);
             } else {
                 history.add(new Task(input));
                 System.out.println("added: " + input);

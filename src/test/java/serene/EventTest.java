e41712b040e96bfa9b7ea2f2895020b789223f05
Charlize <E1354638@u.nus.edu.sg>
2025-08-28 18:52:55 +0800

    Add toSaveFormat test for method in EVent class

---
+++ src/test/java/serene/EventTest.java
@@ -0,0 +1,14 @@
+package serene;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EventTest {
+    @Test
+    public void toSaveFormat() {
+        Event e = new Event("Meeting", "2025-08-28 18:09", "2025-08-28 19:09");
+
+        assertEquals("E , 0 , Meeting , 2025-08-28 18:09 /to 2025-08-28 19:09", e.toSaveFormat());
+    }
+}
---
+++ src/test/java/serene/ParserTest.java
@@ -0,0 +1,4 @@
+package serene;
+
+public class ParserTest {
+}

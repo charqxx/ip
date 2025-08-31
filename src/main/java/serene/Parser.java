75e1f6c1946aa754120775a06c5b7691192cfc45
Charlize <E1354638@u.nus.edu.sg>
2025-08-31 14:21:06 +0800

    Fix conflicts from merging

--- src/main/java/serene/Parser.java
+++ src/main/java/serene/Parser.java
@@ -15,42 +15,6 @@ public class Parser {
         String command = parts[0];
 
         switch (command) {
-<<<<<<< HEAD
-        case "":
-            return new Command(CommandType.EMPTY);
-        case "list":
-            return new Command(CommandType.LIST);
-        case "bye":
-            return new Command(CommandType.BYE);
-        case "mark": {
-            String index = parts[1];
-            return new Command(CommandType.MARK, List.of(index));
-        }
-        case "unmark": {
-            String index = parts[1];
-            return new Command(CommandType.UNMARK, List.of(index));
-        }
-        case "delete": {
-            String index = parts[1];
-            return new Command(CommandType.DELETE, List.of(index));
-        }
-        case "todo":
-            return new Command(CommandType.TODO, List.of(parts[1]));
-        case "deadline": {
-            String task = parts[1].split(" /by ")[0];
-            String date = parts[1].split(" /by ")[1];
-            return new Command(CommandType.DEADLINE, List.of(task, date));
-        }
-        case "event": {
-            String task = parts[1].split(" /from ")[0];
-            String fromTo = parts[1].split(" /from ")[1];
-            String from = fromTo.split(" /to ")[0];
-            String to = fromTo.split(" /to ")[1];
-            return new Command(CommandType.EVENT, List.of(task, from, to));
-        }
-        default:
-            throw new SereneException("um...what?");
-=======
             case "":
                 return new Command(CommandType.EMPTY);
             case "list":
@@ -90,7 +54,6 @@ public class Parser {
             }
             default:
                 throw new SereneException("um...what?");
->>>>>>> branch-Level-9
         }
     }
 }

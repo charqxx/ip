ede793de4473277014def898e401150dbaf3181c
Charlize <E1354638@u.nus.edu.sg>
2025-09-10 19:57:41 +0800

    Fix coding style inconsistencies

--- src/main/java/serene/parser/Parser.java
+++ src/main/java/serene/parser/Parser.java
@@ -20,45 +20,45 @@ public class Parser {
         String command = parts[0];
 
         switch (command) {
-            case "":
-                return new Command(CommandType.EMPTY);
-            case "list":
-                return new Command(CommandType.LIST);
-            case "bye":
-                return new Command(CommandType.BYE);
-            case "mark": {
-                String index = parts[1];
-                return new Command(CommandType.MARK, List.of(index));
-            }
-            case "unmark": {
-                String index = parts[1];
-                return new Command(CommandType.UNMARK, List.of(index));
-            }
-            case "delete": {
-                String index = parts[1];
-                return new Command(CommandType.DELETE, List.of(index));
-            }
-            case "todo":
-                return new Command(CommandType.TODO, List.of(parts[1]));
-            case "deadline": {
-                String task = parts[1].split(" /by ")[0];
-                String date = parts[1].split(" /by ")[1];
-                return new Command(CommandType.DEADLINE, List.of(task, date));
-            }
-            case "event": {
-                String task = parts[1].split(" /from ")[0];
-                String fromTo = parts[1].split(" /from ")[1];
-                String from = fromTo.split(" /to ")[0];
-                String to = fromTo.split(" /to ")[1];
-                return new Command(CommandType.EVENT, List.of(task, from, to));
-            }
+        case "":
+            return new Command(CommandType.EMPTY);
+        case "list":
+            return new Command(CommandType.LIST);
+        case "bye":
+            return new Command(CommandType.BYE);
+        case "mark": {
+            String index = parts[1];
+            return new Command(CommandType.MARK, List.of(index));
+        }
+        case "unmark": {
+            String index = parts[1];
+            return new Command(CommandType.UNMARK, List.of(index));
+        }
+        case "delete": {
+            String index = parts[1];
+            return new Command(CommandType.DELETE, List.of(index));
+        }
+        case "todo":
+            return new Command(CommandType.TODO, List.of(parts[1]));
+        case "deadline": {
+            String task = parts[1].split(" /by ")[0];
+            String date = parts[1].split(" /by ")[1];
+            return new Command(CommandType.DEADLINE, List.of(task, date));
+        }
+        case "event": {
+            String task = parts[1].split(" /from ")[0];
+            String fromTo = parts[1].split(" /from ")[1];
+            String from = fromTo.split(" /to ")[0];
+            String to = fromTo.split(" /to ")[1];
+            return new Command(CommandType.EVENT, List.of(task, from, to));
+        }
 
-            case "find": {
-                String[] keywords = parts[1].split(" ");
-                return new Command(CommandType.FIND, Arrays.asList(keywords));
-            }
-            default:
-                throw new SereneException("um...what?");
+        case "find": {
+            String[] keywords = parts[1].split(" ");
+            return new Command(CommandType.FIND, Arrays.asList(keywords));
+        }
+        default:
+            throw new SereneException("um...what?");
         }
     }
 }
